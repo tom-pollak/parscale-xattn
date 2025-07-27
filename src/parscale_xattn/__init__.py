20a1e3c68cd82c2019fe45c1798abf18e71986bc
Tom Pollak <tomp@graphcore.ai>
2025-07-27 14:26:38 +0100

    x

--- src/parscale_xattn/__init__.py
+++ src/parscale_xattn/__init__.py
@@ -1,10 +1,7 @@
 from .configuration_qwen2_parscale import Qwen2ParScaleConfig
 from .modeling_qwen2_parscale import (
-    KwargsForCausalLM,
-    Qwen2ForQuestionAnswering,
-    Qwen2ForSequenceClassification,
-    Qwen2ForTokenClassification,
     Qwen2Model,
     Qwen2ParScaleForCausalLM,
     Qwen2PreTrainedModel,
 )
+from .modeling_base import KwargsForCausalLM
